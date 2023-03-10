b0daacc25719a53065ea9e1fef111abe07105c73
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-03-10 06:42:08 -0500

    feat: :ambulance: Segundo Ejercicio
    Eliminé el anterior problema y puse la solución del ejercicio 2

--- main.js
+++ main.js
@@ -1,28 +1,13 @@
-/*1. Construir el algoritmo para un programa que ingrese tres
-notas de un alumno, si el promedio es menor o igual a 3.9
-mostrar un mensaje "Estudie“, de lo contrario un mensaje que
-diga "becado"*/
+/* 2. Dado un número indicar si es par o impar y si es mayor de 10. */
+let num = prompt ("Dijite un número")
 
-let nota1 = prompt("Ingrese la primera nota del alumno");
-console.log(nota1);
-let nota2 = prompt("Ingrese la segunda nota del alumno");
-console.log(nota2);
-let nota3 = prompt("Ingrese la tercera nota del alumno");
-console.log(nota3);
-
- let Suma = parseFloat(nota1)+parseFloat(nota2)+parseFloat(nota3);
- console.log(nota1,"+",nota2,"+",nota3,"/ 3");
- let Promedio = Suma / 3;
- console.log(Promedio);
+if (num % 2 == 0)
+ {console.log("El número es par");}
+else if(num % 2 !=0 )
+ {console.log("El número es impar");}
+    if (num > 10)
+    {console.log("El número es MAYOR a 10");}
+    else if( num < 10)
+    {console.log("El número es MENOR a 10");}
  
-let resultado = Promedio <= 3.9;
-
-let e1 = ("Su promedio es menor a 3.9; Estudie")
-let b1 = ("Su promedio es mayor a 3.9; Becado")
-
-if (resultado == false)
-{console.log(b1);}
-else if (resultado == true)
-{console.log(e1);}
-
-
+    
