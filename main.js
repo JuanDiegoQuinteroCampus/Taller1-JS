15f565a6165835da520f71a40fd2ded968da55d6
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-03-13 13:22:22 -0500

    feat: :ambulance: Decimo ejercicio
    se le añadio un algoritmo para recoleccion de datos y una sumatoria, ademas del promedio,nmaximo y nminimo

--- main.js
+++ main.js
@@ -1,29 +1,25 @@
-/* 9. N atletas han pasado a finales en salto triple en los juegos
-olímpicos femenino de 2022. Diseñe un programa que pida por
-teclado los nombres de cada atleta finalista y a su vez, sus
-marcas del salto en metros. Informar el nombre de la atleta
-campeona que se quede con la medalla de oro y si rompió
-récord, reportar el pago que será de 500 millones. El récord
-esta en 15,50 metros. */
-let nombre=[];
-let nombreM=[];
-let marcas=[];
-let marcaM=[];
-
-
-let cont =Number (prompt("cuantos atletas han pasado a finales en salto triple?"));
-for (let i = 0;i < cont; i++){
-    nombre = (prompt(`${i+1} Ingrese el nombre del atleta finalista `))
-    marcas = parseFloat(prompt(`${i+1} Ingrese la marca del atleta `))
-
-    if(marcas>marcaM){
-        marcaM=marcas
-        nombreM=nombre
-            }
-    
+/* 10. Desarrolle un programa cíclico que capture un dato
+numérico cada vez, y los vaya acumulando. El programa se
+detiene cuando el usuario digita un cero. El programa debe
+mostrar: LA SUMATORIA DE LOS VALORES, EL VALOR DEL
+PROMEDIO, CUÁNTOS VALORES FUERON DIGITADOS, MAYOR
+VALOR Y MENOR VALOR.*/
+let lista=[];
+let suma=0;
+i=1;
+ do{
+    let x=Number(prompt(`${i++}Ingrese los datos`))
+    if(x!=0)
+    {lista.unshift(x)}
+    else{
+        break;
     }
-    console.log("El atleta: "+nombreM + " ganó la medalla de oro con una marca de "+marcaM);
-    if(marcaM<15)
-    {console.log("El atleta ganó pero no rompio el record de 15,50m");}
-    else if(marcaM>15)
-    {console.log("El atleta rompio el record asi que ha ganado 500 millones");}
+ }while(true);
+ console.log(lista);
+ lista.forEach(function(a){suma += a;});
+console.log(suma);
+promedio=suma/lista.length
+console.log(promedio);
+console.log(lista.length);
+console.log(Math.max(...lista));
+console.log(Math.min(...lista));
