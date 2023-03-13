e293fa9f61025c7b1db6dc80eb76f2dd337f036e
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-03-13 06:21:18 -0500

    feat: :ambulance: Septimo Ejercicio
    Se le añadio un algoritmo que le permita dar la cantidad de articculos y total de precio

--- main.js
+++ main.js
@@ -1,38 +1,19 @@
-/* 6. Construir el algoritmo en Javascript para un programa
-para cualquier cantidad de estudiantes que lea el nombre,
-el sexo y la nota definitiva y halle al estudiante con la mayor
-nota y al estudiante con la menor nota y cuantos eran
-hombres y cuantos mujeres. */
+/* 7. Programa que pida el ingreso del nombre y precio de un artículo y la
+cantidad que lleva el cliente. Mostrar lo que debe pagar el comprador
+en su factura. */
+let nombrea=[];
+let precio=0;
+let cantidad=0;
 
-let nombre=[];
-let nombreM=[];
-let nombrem=[];
-let nota=0;
-let notai=99999;
-let notaM=0;
-let sexo = [];
-let sexoF=0;
-let sexoM=0;
-let cont =Number (prompt("cuantos datos desea ingresa?"));
+let cont =Number (prompt("cuantos articulos desea comprar?"));
 for (let i = 0;i < cont; i++){
-    nombre = (prompt(`${i+1} ingrese el nombre del estudiante `))
-    nota=parseInt(prompt(`${i+1} ingrese la nota definitiva `))
-    if(nota>notaM){
-    notaM=nota
-    nombreM=nombre
-        }
-    if(nota<notai){
-    notai=nota
-    nombrem=nombre
-        }
-    sexo = (prompt(`${i+1} ingrese el sexo de la persona (M-F, recuerde en mayuscula)`))
-    if(sexo=="F")
-        {sexoF += 1}
-    else if(sexo=="M")
-        {sexoM += 1}
-}
-console.log("El estudiante que tuvo la mayor nota fue "+ nombreM + " con una nota de " + notaM);
-console.log("El estudiante que tuvo la menor nota fue "+ nombrem + " con una nota de " + notai);
-console.log(sexoF + " Eran Mujeres ");
-console.log(sexoM + " Eran Hombres macho pecho pelaos");
+    nombrea = (prompt(`${i+1} ingrese el nombre del articulo `))
+    let valor = parseFloat(prompt(`${i+1} ingrese el precio del articulo `))
 
+    
+    cantidad++
+    precio += valor
+
+        
+    }
+    console.log("Debe pagar "+ precio+ " por " + cantidad+ " de articulos");
